bccd03cffc333f9961c52679d58fc14b84ace766
SPrathaban <sprathaban@kingpin.global>
2023-06-21 13:18:44 +0530

    First commit

---
+++ bin/dart_application_training.dart
@@ -0,0 +1,5 @@
+import 'package:dart_application_training/dart_application_training.dart' as dart_application_training;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_training.calculate()}!');
+}
